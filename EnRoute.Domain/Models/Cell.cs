4511cf134ac50e76804936c44805fe1a69a6ef89
Polina Berest <pamapolya@gmail.com>
2024-02-23 22:39:11 +0200

    Added constants, domain models

---
+++ EnRoute.Domain/Models/Cell.cs
@@ -0,0 +1,19 @@
+﻿using EnRoute.Domain.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnRoute.Domain.Models
+{
+    public class Cell : IODataEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsFree { get; set; } = true;
+        public bool hasTemperatureControl { get; set; } = false;
+        public Guid CounterId { get; set; }
+        public PickupCounter Counter { get; set; }
+        public string CellOpenKey { get; set; } = DateTime.UtcNow.ToString();
+    }
+}
